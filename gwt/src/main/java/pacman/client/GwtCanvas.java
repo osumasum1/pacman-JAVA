ec7307b7c22fe01413388bc852d894f6eb550e67
Alejandro Castilla <acastillaquesada@gmail.com>
2016-06-26 19:59:06 +0200

    Working on GWT module. Game logic works but canvas is not being drawn correctly (needs to be fixed).

--- gwt/src/main/java/pacman/client/GwtCanvas.java
+++ gwt/src/main/java/pacman/client/GwtCanvas.java
@@ -1,33 +1,62 @@
 package pacman.client;
 
+import com.google.gwt.canvas.dom.client.Context2d;
 import pacman.shared.Canvas;
 
-public class GwtCanvas  implements Canvas{
+public class GwtCanvas implements Canvas {
+
+    private com.google.gwt.canvas.client.Canvas canvas;
+    private Context2d c2d;
+
+    public GwtCanvas (com.google.gwt.canvas.client.Canvas canvas) {
+        this.canvas = canvas;
+        this.c2d = canvas.getContext2d();
+    }
+
     @Override public double getWidth() {
-        throw new UnsupportedOperationException("Not implemented");
+        return canvas.getCoordinateSpaceWidth();
     }
+
     @Override public double getHeight() {
-        throw new UnsupportedOperationException("Not implemented");
+        return canvas.getCoordinateSpaceHeight();
     }
+
     @Override public void beginPath() {
-        throw new UnsupportedOperationException("Not implemented");
+        c2d.beginPath();
     }
+
     @Override public void closePath() {
-        throw new UnsupportedOperationException("Not implemented");
+        c2d.closePath();
     }
+
     @Override public void setFill(String color) {
-        throw new UnsupportedOperationException("Not implemented");
+        c2d.setFillStyle(color);
     }
+
     @Override public void fillRect(double x, double y, double w, double h) {
-        throw new UnsupportedOperationException("Not implemented");
+        c2d.fillRect(x, y, w, h);
     }
+
     @Override public void fillPolygon(double[] xPoints, double[] yPoints, int nPoints) {
-        throw new UnsupportedOperationException("Not implemented");
+        for (int i = 0 ; i < nPoints ; i++) {
+            c2d.moveTo(xPoints[i], yPoints[i]);
+
+            if (i+1 < nPoints) {
+                c2d.lineTo(xPoints[i+1], yPoints[i+1]);
+            } else {
+                c2d.lineTo(xPoints[0], yPoints[0]);
+            }
+        }
+
+        c2d.fill();
     }
+
     @Override public void fillArc(double x, double y, double w, double h, double startAngle, double arcExtent) {
-        throw new UnsupportedOperationException("Not implemented");
+        //Radius is width/2 = (cw + 2)/2
+        c2d.arc(x, y, w/2, startAngle, arcExtent);
     }
+
     @Override public void fillOval(double x, double y, double w, double h) {
-        throw new UnsupportedOperationException("Not implemented");
+        c2d.arc(x, y, w/2, 0, Math.PI * 2);
     }
 }
--- gwt/src/main/java/pacman/client/GwtGame.java
+++ gwt/src/main/java/pacman/client/GwtGame.java
@@ -1,36 +1,106 @@
 package pacman.client;
 
 import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.RootPanel;
+import pacman.shared.Drawer;
 import pacman.shared.MainLoop;
+import pacman.shared.Maze;
 
 public class GwtGame implements EntryPoint {
+
+    private final String BAR_CONTAINER = "barContainer";
+    private final String CANVAS_CONTAINER = "canvasContainer";
+
+    private final int CANVAS_WIDTH = 700;
+    private final int CANVAS_HEIGHT = 700;
+
+    private MainLoop game;
+    private Timer ticker;
+
     @Override public void onModuleLoad() {
-        // the label can be used to show the score --v
-        RootPanel.get().add(new Label("Main loop instantiated (so it works in js): " + new MainLoop(null, 1)));
+        Label labelScore = new Label("Score: 0");
+        RootPanel.get().add(labelScore);
 
-        Button exit = new Button("Exit XD");
-        exit.addClickHandler(click -> {
-            // this can be used to show simple text dialogs
-            Window.alert("not sure you can exit from a web page!");
-        });
-        RootPanel.get().add(exit);
+        Drawer.Toaster score = s -> labelScore.setText("Score: " + s);
+        Drawer.Toaster alert = text -> Window.alert(text);
 
+        //Create canvas
         Canvas canvas = Canvas.createIfSupported();
         if (canvas == null) {
             Window.alert("Ups! your browser do not support canvas...");
             return;
         }
-        canvas.setHeight("300px");
-        canvas.setWidth("300px");
-        Context2d c2d = canvas.getContext2d();
-        c2d.setFillStyle("blue");
-        c2d.fillRect(0,0,100,100);
-        RootPanel.get().add(canvas);
+
+        canvas.setWidth(CANVAS_WIDTH + "px");
+        canvas.setHeight(CANVAS_HEIGHT + "px");
+        canvas.setCoordinateSpaceWidth(CANVAS_WIDTH);
+        canvas.setCoordinateSpaceHeight(CANVAS_HEIGHT);
+
+        canvas.addKeyDownHandler(event -> {
+            switch (event.getNativeKeyCode()) {
+                case KeyCodes.KEY_UP: game.pacMan.nextDirection = Maze.UP; break;
+                case KeyCodes.KEY_DOWN: game.pacMan.nextDirection = Maze.DOWN; break;
+                case KeyCodes.KEY_LEFT: game.pacMan.nextDirection = Maze.LEFT; break;
+                case KeyCodes.KEY_RIGHT: game.pacMan.nextDirection = Maze.RIGHT; break;
+                case KeyCodes.KEY_SPACE: playPause(GwtGame.this.game); break;
+            }
+        });
+
+        canvas.addMouseOverHandler(event -> canvas.setFocus(true));
+
+        //Set drawer and menu bar
+        Drawer drawer = new Drawer(new GwtCanvas(canvas), score, alert);
+        RootPanel.get(CANVAS_CONTAINER).add(canvas);
+        Runnable restart = () -> restart(drawer);
+
+        Command newGame = () -> restart.run();
+        Command pauseGame = () -> playPause(game);
+
+        MenuBar bar = new MenuBar(true);
+        bar.addItem("Nuevo", newGame);
+        bar.addItem("Pausar", pauseGame);
+
+        MenuBar rootBar = new MenuBar();
+        rootBar.addItem("Juego", bar);
+
+        RootPanel.get(BAR_CONTAINER).add(rootBar);
+
+        canvas.setFocus(true);
+
+        restart.run();
+
     }
+
+    public void restart(Drawer drawer) {
+        if (game != null) {
+            game.endGame();
+            ticker = null;
+        }
+        game = new MainLoop(drawer, 0);
+        playPause(GwtGame.this.game); // start ticker
+    }
+
+    public void playPause(MainLoop game) {
+        if (ticker == null) {  // play
+            ticker = new Timer() {
+                @Override
+                public void run() {
+                    if (game.gameEnded) cancel();
+                    else game.tick();
+                }
+            };
+            ticker.scheduleRepeating(game.delta);
+        } else {  // pause
+            ticker.cancel();
+            ticker = null;
+        }
+    }
+
 }
